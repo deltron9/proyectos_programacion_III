3bb0ac287eb9217065a777aa1f0a8ef13608e4ca
deltron9 <you@example.com>
2025-10-12 22:38:05 -0300

    Resuelto ejercicio 3

---
+++ ejercicios_JS/ejercicio3/ejercicio3.js
@@ -0,0 +1,6 @@
+let meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+
+meses.forEach(elemento => {
+    const elementos = elemento + " - numero de mes: " + elementos.length;
+    console.log(elementos);
+});
