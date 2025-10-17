b88749a51c2f84677fccf84e81e583605d62401e
deltron9 <cristian_collante@hotmail.com>
2025-10-17 00:25:31 -0300

    Resolución ejercicio 4

--- ejercicios_JS/ejercicio4/ejercicio4.js
+++ ejercicios_JS/ejercicio4/ejercicio4.js
@@ -1,3 +1,14 @@
 /*
-Realizar una función que reciba un número y que muestre (por consola) un mensaje como el siguiente: "El número 5 es impar", siendo 5 el número recibido como parámetro.
+Realizar una función que reciba un número y que muestre (por consola) un mensaje como 
+el siguiente: "El número 5 es impar", siendo 5 el número recibido como parámetro.
 */
+function validarImpar(numero){
+if(numero % 2 === 1){
+    return `${numero} es impar`;
+}
+else{
+    return `${numero} es par`;
+}
+}
+
+console.log(validarImpar(13))
