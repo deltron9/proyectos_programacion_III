52fa6d77f7479f63d6719f1484d8757f779b5f30
deltron9 <cristian_collante@hotmail.com>
2025-10-19 11:19:03 -0300

    resolucion ejercicio 5

--- ejercicios_JS/ejercicio5/ejercicio5.js
+++ ejercicios_JS/ejercicio5/ejercicio5.js
@@ -3,3 +3,19 @@ Realizar una función que reciba un parámetro requerido de tipo numérico y otr
 Si el segundo parámetro es recibido, se mostrará tantas veces por consola, como lo indique el primer parámetro.
 En caso de no recibir el segundo parámetro, se mostrará el valor inverso del primer parámetro. 
 */
+function devolverElementos(numero, valor2){
+    let = numero
+    let = valor2
+    if(valor2){
+        for(let i = 0; i < numero; i++){
+            console.log(valor2)
+        }
+    }
+    else{
+        console.log(numero - (numero * 2))
+        }
+    }
+    
+    devolverElementos(12, "SEXO")
+    console.log("--------------------------------------------------------------------------------------")
+    devolverElementos(1)
