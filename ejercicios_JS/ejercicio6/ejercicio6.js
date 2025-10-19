60f2eb0b16813f3861a3dd9ff21bbf04ad4da719
deltron9 <cristian_collante@hotmail.com>
2025-10-19 11:46:47 -0300

    resolucion ejercicio 6

--- ejercicios_JS/ejercicio6/ejercicio6.js
+++ ejercicios_JS/ejercicio6/ejercicio6.js
@@ -5,3 +5,16 @@ Mostrará el apellido en mayúscula.
 Mostrará el nombre solo con la primera letra en mayúsculas y el resto en minúsculas.
 El apellido y el nombre se mostrarán separados por una coma (,). 
 */
+nombre = "apu"
+apellido = "nahasapemapetilon"
+
+function mostrarNombreApellido(nombre, apellido){
+
+const nombreCap = nombre.charAt(0).toUpperCase();
+const nombreMin = nombre.slice(1).toLowerCase();
+const nombreCom = nombreCap + nombreMin
+const apellidoMa = apellido.toUpperCase();
+console.log(`${nombreCom}, ${apellidoMa}`);
+
+}
+mostrarNombreApellido(nombre, apellido)
