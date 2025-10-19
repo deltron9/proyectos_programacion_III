866f1a50b9af2e819e0dadbe0bd06a0996f9e2ec
deltron9 <cristian_collante@hotmail.com>
2025-10-19 11:57:40 -0300

    resolucion ejercicio 7

--- ejercicios_JS/ejercicio7/ejercicio7.js
+++ ejercicios_JS/ejercicio7/ejercicio7.js
@@ -4,3 +4,17 @@ si excede longitud, reemplaza el final de la cadena con el carácter de puntos s
 para hacer su longitud igual al parámetro longitud. 
 El resultado de la función debe ser la cadena truncada (si es necesario).
 */
+function truncate(cadena, longitud) {
+    if (cadena.length <= longitud) {
+        return cadena;
+    }
+
+    const longPuntos = 3;
+    const puntoDeCorte = longitud - longPuntos;
+
+    const cadenaTruncada = cadena.slice(0, puntoDeCorte);
+
+    return cadenaTruncada + "...";
+}
+
+console.log(truncate("nahassapemapetilon", 12));
