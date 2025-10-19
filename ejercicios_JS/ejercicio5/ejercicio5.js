d789d820f2e1ecef969c8be87860dab8a9271559
deltron9 <cristian_collante@hotmail.com>
2025-10-19 11:21:16 -0300

    modificacion

--- ejercicios_JS/ejercicio5/ejercicio5.js
+++ ejercicios_JS/ejercicio5/ejercicio5.js
@@ -4,10 +4,8 @@ Si el segundo parámetro es recibido, se mostrará tantas veces por consola, com
 En caso de no recibir el segundo parámetro, se mostrará el valor inverso del primer parámetro. 
 */
 function devolverElementos(numero, valor2){
-    let = numero
-    let = valor2
     if(valor2){
-        for(let i = 0; i < numero; i++){
+        for(let i = 0 ; i < numero ; i++){
             console.log(valor2)
         }
     }
@@ -15,7 +13,7 @@ function devolverElementos(numero, valor2){
         console.log(numero - (numero * 2))
         }
     }
-    
+
     devolverElementos(12, "SEXO")
     console.log("--------------------------------------------------------------------------------------")
     devolverElementos(1)
