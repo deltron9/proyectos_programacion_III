c800926a9f4db86518a11bf6313e086dd89d39d3
deltron9 <cristian_collante@hotmail.com>
2025-10-28 01:38:06 -0300

    ADD: nuevas carpetas de ejercicios

--- ejercicios_JS/ejercicio9/ejercicio9.js
+++ ejercicios_JS/ejercicio9/ejercicio9.js
@@ -1,4 +1,6 @@
 /*
-Usando el operador spread (...), crea una copia de un array y agrega un nuevo elemento al final.
-*Ejemplo original: const frutas = ["manzana", "banana"]; *Resultado: ["manzana", "banana", "naranja"] 
+Usando el operador spread (...), 
+crea una copia de un array y agrega un nuevo elemento al final.
+*Ejemplo original: const frutas = ["manzana", "banana"]; 
+*Resultado: ["manzana", "banana", "naranja"] 
 */
